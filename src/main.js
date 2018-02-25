66e4e583f7bb8527d50531acc30265d296afde6c
Pool-chan <poolchan.me@gmail.com>
2018-02-26 03:26:37 +0900

    🔨 add import VueI18n library

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+Vue.use(VueI18n)
 new Vue({
   el: '#app',
   components: { App },
