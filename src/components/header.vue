e96caa7721111093ff84348784e9eb99a5b7187c
Pool-chan <poolchan.me@gmail.com>
2018-03-02 00:50:12 +0900

    💄 update header.vue layout and style

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,15 @@
 <template>
   <header id="header">
-    <h1>Header</h1>
+    <div class="container">
+      <h1>MinerGuide<span class="sub">Cryptocurrency Miner setting guide</span></h1>
+      <nav id="header-nav">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Setting</a></li>
+          <li><a href="#">Help</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
