3a1960f6446fa2fdc7da01dbe8248953bd16df34
Pool-chan <poolchan.me@gmail.com>
2018-03-02 03:29:08 +0900

    🚧 !Progress! footer design works

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,5 +1,14 @@
 <template>
   <footer id="footer">
-    <h1>Footer</h1>
+    <div class="container">
+      <p><span class="disclaimer">This web application doesn't send any data on outside/remote server.</span>
+      <br />Made with ❤️ by <a href="https://github.com/pool-chan">Pool-chan</a></p>
+      <nav class="footer-nav">
+        <ul>
+          <li><a href="#">Github</a></li>
+          <li><a href="#">Mining Pool</a></li>
+        </ul>
+      </nav>
+    </div>
   </footer>
 </template>
