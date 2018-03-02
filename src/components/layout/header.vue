5fb79e7c13c1a1a89d6540afef33b1674fb696a9
Pool-chan <poolchan.me@gmail.com>
2018-03-02 20:52:00 +0900

    💄 remove some navigation sections

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -4,8 +4,6 @@
       <h1>MinerGuide<span class="sub">Cryptocurrency Miner setting guide</span></h1>
       <nav id="header-nav">
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Setting</a></li>
           <li><a href="#">Help</a></li>
           <li><a href="#">About</a></li>
         </ul>
