f72fc6a84011ccd5ec83333a159a97bba58a9d2b
Pool-chan <poolchan.me@gmail.com>
2018-03-02 20:16:52 +0900

    💄 working complete footer design

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer id="footer">
     <div class="container">
-      <p><span class="disclaimer">This web application doesn't send any data on outside/remote server.</span>
+      <p class="copyright"><span class="disclaimer">This web application doesn't send any data on outside/remote server.</span>
       <br />Made with ❤️ by <a href="https://github.com/pool-chan">Pool-chan</a></p>
       <nav class="footer-nav">
         <ul>
