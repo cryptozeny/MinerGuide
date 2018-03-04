41d61951ee25986221d0a8c3d12486600d1ddf99
Pool-chan <poolchan.me@gmail.com>
2018-03-05 03:02:50 +0900

    🎨 structure fix about setup section.

--- src/components/setup/default.vue
+++ src/components/setup/default.vue
@@ -1,8 +1,36 @@
 <template>
   <section id="setup">
-    <h1>Setup</h1>
     <transition name="setup">
-      <Introduce />
+      <Introduce v-if="$parent.setup.currentStep === 0" />
+      <Os v-if="$parent.setup.currentStep === 1" />
+      <SystemResource v-if="$parent.setup.currentStep === 2" />
+      <MiningPool v-if="$parent.setup.currentStep === 3" />
     </transition>
+    <a href="#" @click.prevent="getSystemInformation">Button</a>
   </section>
 </template>
+
+<script>
+import Introduce from './introduce.vue'
+import Os from './os.vue'
+import SystemResource from './system_resource.vue'
+import MiningPool from './mining_pool.vue'
+
+export default {
+  components: {
+    Introduce,
+    Os,
+    SystemResource,
+    MiningPool
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    getSystemInformation () {
+      this.$parent.result.OS = navigator.platform
+      this.$parent.setup.availableCPUThread = navigator.hardwareConcurrency
+    }
+  }
+}
+</script>
