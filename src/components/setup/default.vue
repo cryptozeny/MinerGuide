c1d5774acf349990c6cdcc5df335ae81ea72b5c7
Pool-chan <poolchan.me@gmail.com>
2018-03-07 05:05:09 +0900

    :art: add step return navigation

--- src/components/setup/default.vue
+++ src/components/setup/default.vue
@@ -1,5 +1,9 @@
 <template>
   <section id="setup">
+    <a id="setupStepReset"
+       href="#"
+       v-if="$parent.setup.currentStep !== 0"
+       @click.prevent="returnBeforeStep">이전 단계로 돌아가기</a>
     <transition name="setup">
       <Introduce v-if="$parent.setup.currentStep === 0" />
       <Os v-if="$parent.setup.currentStep === 1" />
@@ -22,14 +26,26 @@ export default {
     SystemResource,
     MiningPool
   },
-  data () {
-    return {}
-  },
   methods: {
-    getSystemInformation () {
-      this.$parent.result.OS = navigator.platform
-      this.$parent.setup.availableCPUThread = navigator.hardwareConcurrency
+    returnBeforeStep () {
+      if (this.$parent.setup.currentStep > 0) {
+        this.$parent.setup.currentStep = this.$parent.setup.currentStep - 1
+        return true
+      } else {
+        return false
+      }
     }
   }
 }
 </script>
+
+<style lang="scss">
+  #setupStepReset {
+    display:block;
+    text-align:center;
+    margin:-1rem 0 0;
+    padding:1rem 0;
+    border-bottom:1px solid $oc-gray-2;
+    color:$oc-gray-9;
+  }
+</style>
