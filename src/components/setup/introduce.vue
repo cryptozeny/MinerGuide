f1ba270f136ab03b6087f3e93f5e44bc0da81c64
Pool-chan <poolchan.me@gmail.com>
2018-03-07 02:33:33 +0900

    :sparkles: add setup/introduce.vue component
    this component is function into intro of setup.

--- src/components/setup/introduce.vue
+++ src/components/setup/introduce.vue
@@ -7,9 +7,9 @@
     </p>
 
     <h2>어떤 기계로 마이닝 하실건가요?</h2>
-    <div class="selectButtons">
-      <div class="pc-button pc-button_large"
-           @click.prevent="$parent.$parent"
+    <div class="pc-box-flex pc-flex-row">
+      <div class="pc-button pc-button-large"
+           @click.prevent="AcceptCurrentEnv"
            :disabled="usingOS === false">
         <div class="wrap">
           <i v-if="usingOS !== false"
@@ -19,7 +19,7 @@
           <span class="description">자동으로 설정이 맞춰집니다.</span>
         </div>
       </div>
-      <div class="pc-button" @click.prevent="$parent.$parent">
+      <div class="pc-button pc-button-large" @click.prevent="$parent.$parent.setup.currentStep = 1">
         <div class="wrap">
           <i class="fas fa-desktop"></i>
           <h3>다른 임의의 기기</h3>
@@ -49,70 +49,35 @@ export default {
     } else if (currentOS.indexOf('Linux')) {
       this.usingOS = 'linux'
     }
-
-    this.usingOS = false
+  },
+  methods: {
+    AcceptCurrentEnv () {
+      this.$parent.$parent.result.OS = navigator.platform
+      this.$parent.$parent.setup.availableCPUThread = navigator.hardwareConcurrency
+      this.$parent.$parent.setup.currentStep = 2
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .selectButtons {
-    width:100%;
-    display:flex;
-    flex-direction:row;
-
-    & > div {
-      position:relative;
-      display:flex;
-      flex:1;
-      flex-direction:row;
-      width:100%;
-      margin:0 12px;
-      height:360px;
-      vertical-align:middle;
-      border:1px solid $oc-blue-3;
-      cursor:pointer;
-      border-radius:16px;
-      overflow:hidden;
-      box-shadow:0 4px 4px $oc-gray-2;
-
-      transition:all 0.132s ease-in-out;
-
-      & > div.wrap {
-        align-self:center;
-        text-align:center;
-        width:100%;
-      }
-
-      &[disabled="disabled"] {
-        border-color:$oc-gray-4;
-        opacity:0.7;
-        cursor:default;
-
-        &::after {
-          position:absolute;
-          width:100%;
-          height:100%;
-          text-align:center;
-          background-color:rgba(255, 255, 255, 0.75);
-          left:0;
-          top:0;
-          line-height:360px;
-          font-weight:bold;
-          font-size:14pt;
-          content:"해당 기기에서는 마이닝이 불가능합니다";
-        }
-      }
+  .pc-button {
+    .fab, .fas {
+      font-size:48px;
+      margin:0 0 1rem;
     }
 
-    & > div:hover {
-      border-color:$oc-blue-5;
-      width:110%;
-      box-shadow:0 4px 4px $oc-gray-3;
+    h3 {
+      font-size:2rem;
+      margin:0;
+      color:$oc-gray-8;
     }
 
     span.description {
       display:block;
+      margin:0.25rem 0 0;
+      font-size:1.25rem;
+      color:$oc-gray-6;
     }
   }
 </style>
