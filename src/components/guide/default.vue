50c542cc853813cc5aac0e2f97948e82cea83db7
Pool-chan <poolchan.me@gmail.com>
2018-03-13 17:52:43 +0900

    :memo: writing ko/windows guide

--- src/components/guide/default.vue
+++ src/components/guide/default.vue
@@ -8,7 +8,8 @@
     </div>
 
     <div class="hero">
-      <p>꼭 해당 설정파일을 마이너가 위치할 폴더와 같은곳에 넣어주세요</p>
+      <h2>설정파일 다운로드</h2>
+      <p>꼭 해당 설정파일을 마이너 프로그램이 들어있는 폴더와 같은곳에 넣어주세요</p>
       <a class="pc-button"
          :href="'data:text/json;charset=utf-8,' + returnConfigJSON()"
          download="config.json">설정 파일 다운로드</a>
@@ -21,9 +22,10 @@
     <div class="hero">
       <p>해당 실행 스크립트를 마이너가 위치할 폴더와 같은곳에 넣어주세요</p>
       <a class="pc-button"
-         :href="'data:application/octet-stream;charset=utf-8,' + makeRunScript()"
+         :href="'data:application/octet-stream;charset=utf-8,' + makeRunScript(true)"
          :download="makeRunScriptFilename()">실행스크립트 파일 다운로드</a>
       <p>실행 스크립트는 운영체제에 직접적인 영향을 줄 수 있습니다. 꼭 내용이 아래와 일치하는지 확인 후 다운로드를 진행하세요</p>
+      <code>{{ makeRunScript(false) }}</code>
     </div>
 
     <div class="markdown-content"
@@ -41,15 +43,14 @@ export default {
         'user': this.$parent.result.pool.worker,
         'pass': this.$parent.result.pool.password,
         'algo': 'yescrypt',
-        'quiet': 'true',
         'cpu-affinity': this.$parent.result.CPUAffinity
       }))
     },
     makeRunScript (isEscape) {
-      let ShellScript = ''
+      let ShellScript = './minerd260 --config=config.json'
 
       if (this.$parent.result.OS === 'windows') {
-        ShellScript = 'cpuminer260.exe --config=config.json'
+        ShellScript = ShellScript.substring(2)
       }
 
       if (isEscape === true) {
@@ -63,10 +64,37 @@ export default {
       return MarkdownRequire(`./${this.$parent.Lang}/guide/${this.$parent.result.OS}/${filePath}.md`)
     },
     makeRunScriptFilename () {
-
+      if (this.$parent.result.OS === 'windows') {
+        return 'run-miner.bat'
+      } else {
+        return 'run-miner.sh'
+      }
     }
   },
   mounted () {
   }
 }
 </script>
+
+<style lang="scss">
+.markdown-content img {
+  display:flex;
+  align-self:center;
+  max-width:100%;
+  margin:0 auto;
+  height:auto;
+}
+
+.markdown-content code,
+.hero code {
+  display:inline-block;
+  background-color:$oc-gray-6;
+  color:#fff;
+  font-size:14px;
+  font-weight:bold;
+  font-family:'San-serif';
+  padding:6px 12px;
+  vertical-align:middle;
+  border-radius:4px;
+}
+</style>
--- src/markdown/ko/guide/windows/download.md
+++ src/markdown/ko/guide/windows/download.md
@@ -1,5 +1,19 @@
 ## 마이너 다운로드
 
-마이너는
+마이너 프로그램은 보안을 위하여 직접 제공해드리지 않습니다. 아래의 링크에 들어가셔서 다운로드 해주세요.
 
-<a href="#" class="pc-button pc-button-reverse" target="_blank">마이너 프로그램 다운로드 하기</a>
+<a href="https://github.com/macchky/cpuminer/releases/tag/v2.6.0" class="pc-button pc-button-reverse" target="_blank">마이너 프로그램 다운로드 하기</a>
+
+해당 링크에 들어가신 뒤, 페이지 항목중에 ``ZNYminer260.zip`` 을 받아주세요.
+
+![다운로드 항목 안내](/static/img/guide/windows/01.png)
+
+다운로드 받으신 ``ZNYminer260.zip`` 을 여신 뒤, ``linux`` 폴더를 제외한 모든 항목을 새로운 폴더에 압축해제해줍니다.
+
+![압축파일 목록 안내](/static/img/guide/windows/02.png)
+
+압축해제가 완료되었다면, 폴더안에는 다음과 같은 항목들이 들어있어야합니다.
+
+![압축해제 결과](/static/img/guide/windows/03.png)
+
+그 다음에는 아래 설정파일 다운로드를 통하여 설정파일이 같은 폴더안에 설치되어야합니다.
