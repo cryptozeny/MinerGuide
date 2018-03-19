0582c6a59fb3b8c3cd02038aaa648d76cdcf6fce
Pool-chan <poolchan.me@gmail.com>
2018-03-20 03:58:37 +0900

    :globe_with_meridians: support i18n!(fully?)

--- src/components/guide/windows.vue
+++ src/components/guide/windows.vue
@@ -11,12 +11,12 @@
          v-html="$parent.importMarkdown('install')">
     </div>
 
-    <div class="hero">
-      <p>해당 실행 스크립트를 마이너가 위치할 폴더와 같은곳에 넣어주세요</p>
+    <div class="download-container">
+      <p>{{ $t('guide_script_description') }}</p>
       <a class="pc-button"
          :href="'data:application/octet-stream;charset=utf-8,' + $parent.makeRunScript(true)"
-         :download="$parent.makeRunScriptFilename()">실행스크립트 파일 다운로드</a>
-      <p>실행 스크립트는 운영체제에 직접적인 영향을 줄 수 있습니다. 꼭 내용이 아래와 일치하는지 확인 후 다운로드를 진행하세요</p>
+         :download="$parent.makeRunScriptFilename()">{{ $t('guide_script_button') }}</a>
+      <p>{{ $t('guide_script_alert') }}</p>
       <code>{{ $parent.makeRunScript(false) }}</code>
     </div>
 
