07528bda1f22b193e74b60540975a984c9e07eb3
Pool-chan <poolchan.me@gmail.com>
2018-03-25 03:31:29 +0900

    :lipstick: improve select language section

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -6,9 +6,33 @@
         <ul>
           <li><a href="#">Help</a></li>
           <li><a href="#">About</a></li>
-          <li><a href="#">Select Language</a></li>
+          <li class="lang">
+            <a href="#">Select Language</a>
+            <ul>
+              <li :class="{active: $i18n.locale === 'ko'}">
+                <a href="#" @click.prevent="changeLanguage('ko')">한국어</a>
+              </li>
+              <li :class="{active: $i18n.locale === 'jp'}">
+                <a href="#" @click.prevent="changeLanguage('jp')">日本語</a>
+              </li>
+              <li :class="{active: $i18n.locale === 'en'}">
+                <a href="#" @click.prevent="changeLanguage('en')">English</a>
+              </li>
+            </ul>
+          </li>
         </ul>
       </nav>
     </div>
   </header>
 </template>
+
+<script>
+export default {
+  methods: {
+    changeLanguage (Lang) {
+      this.$i18n.locale = Lang
+      this.$parent.Lang = Lang
+    }
+  }
+}
+</script>
